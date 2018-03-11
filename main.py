6c8b060c30de149088433d49135ff7c0d6c03e86
Fernando Martins <fernando@MacBook-Pro-de-Fernando.local>
2018-03-11 16:12:01 -0300

    adding mongo db config

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from helpers.instrument_helper import InstrumentHelper
 import time
 import sys
+import pymongo
+from pymongo import MongoClient
 
 params = sys.argv
 
+client = MongoClient()
+client = MongoClient('mongodb://tcc2:root@ds211029.mlab.com:11029/historical_eur_usd')
+db = client['historical_eur_usd']
+collection = db['eur_usd']
 
 if(len(sys.argv[1:])):
 	_instrument = str(params[1])
@@ -14,5 +20,8 @@ else:
 
 instrument_helper = InstrumentHelper()
 while True:
-	print instrument_helper.get_info(_instrument)
+	data = instrument_helper.get_info(_instrument)
+	print data
+	data_id = collection.insert_one(data).inserted_id
+	print data_id
 	time.sleep(float(_period))
