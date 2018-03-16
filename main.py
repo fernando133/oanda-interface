32b20844c448746dfe779ba1e32a6023c19b7bb5
Fernando Martins <fernando@MacBook-Pro-de-Fernando.local>
2018-03-16 13:46:55 -0300

    ajustando coleta de dados

--- main.py
+++ main.py
@@ -9,19 +9,19 @@ params = sys.argv
 client = MongoClient()
 client = MongoClient('mongodb://tcc2:root@ds211029.mlab.com:11029/historical_eur_usd')
 db = client['historical_eur_usd']
-collection = db['eur_usd']
+collection = db['eur_usd_2']
 
 if(len(sys.argv[1:])):
-	_instrument = str(params[1])
-	_period = str(params[2])
+	instrument = str(params[1])
+	period = str(params[2])
 else:
-	_instrument = 'EUR_USD'
-	_period = 60
+	instrument = 'EUR_USD'
+	period = 60
 
 instrument_helper = InstrumentHelper()
 while True:
-	data = instrument_helper.get_info(_instrument)
-	print data
-	data_id = collection.insert_one(data).inserted_id
+	data = instrument_helper.get_info(instrument)
+	print data['prices'][0]
+	data_id = collection.insert_one(data['prices'][0]).inserted_id
 	print data_id
-	time.sleep(float(_period))
+	time.sleep(float(period))
